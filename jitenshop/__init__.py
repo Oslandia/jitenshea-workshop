ce3bf704cba818596acd4c4995323d444e5020ed
Raphaël Delhome <raphael.delhome@oslandia.com>
2019-08-12 15:13:26 +0200

    init: restaure the jitenshea logger and configuration file handling

--- jitenshop/__init__.py
+++ jitenshop/__init__.py
@@ -1,1 +1,32 @@
 __version__ = 0.1
+
+import os
+import logging
+import configparser
+
+import daiquiri
+import daiquiri.formatter
+
+
+_ROOT = os.path.dirname(os.path.abspath(__file__))
+_CONFIG = os.path.join(_ROOT, 'config.ini')
+
+
+FORMAT = (
+    "%(asctime)s :: %(color)s%(levelname)s :: %(name)s :: %(funcName)s :"
+    "%(message)s%(color_stop)s"
+    )
+daiquiri.setup(level=logging.INFO, outputs=(
+    daiquiri.output.Stream(formatter=daiquiri.formatter.ColorFormatter(
+        fmt=FORMAT)),
+    ))
+logger = daiquiri.getLogger("root")
+
+
+if not os.path.isfile(_CONFIG):
+    logger.error("Configuration file '%s' not found", _CONFIG)
+    config = None
+else:
+    config = configparser.ConfigParser(allow_no_value=True)
+    with open(_CONFIG) as fobj:
+        config.read_file(fobj)
