08e3f03106c00d09fbd27c9a0245b2b1f2ca4b9b
Raphaël Delhome <raphael.delhome@oslandia.com>
2019-08-14 11:32:11 +0200

    tasks: move clustering materials into relevant task module

--- jitenshop/tasks/clusters.py
+++ jitenshop/tasks/clusters.py
@@ -8,13 +8,77 @@ import luigi
 from luigi.contrib.postgres import CopyToTable
 from luigi.format import MixedUnicodeBytes
 import pandas as pd
+from sklearn.cluster import KMeans
 
-from jitenshop import clusters, config, iodb
+from jitenshop import config, iodb
 from jitenshop.tasks import availability
 from jitenshop.tasks import one_week_ago
 
 
 DATADIR = config["main"]["datadir"]
+
+
+def preprocess_data_for_clustering(df):
+    """Prepare data in order to apply a clustering algorithm
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input data, *i.e.* city-related timeseries, supposed to have
+    `station_id`, `ts` and `nb_bikes` columns
+
+    Returns
+    -------
+    pandas.DataFrame
+        Simpified version of `df`, ready to be used for clustering
+
+    """
+    # Filter unactive stations
+    max_bikes = df.groupby("station_id")["nb_bikes"].max()
+    unactive_stations = max_bikes[max_bikes == 0].index.tolist()
+    df = df[~ df['station_id'].isin(unactive_stations)]
+    # Set timestamps as the DataFrame index
+    # and resample it with 5-minute periods
+    df = (df.set_index("ts")
+          .groupby("station_id")["nb_bikes"]
+          .resample("5T")
+          .mean()
+          .bfill())
+    df = df.unstack(0)
+    # Drop week-end records
+    df = df[df.index.weekday < 5]
+    # Gather data regarding hour of the day
+    df['hour'] = df.index.hour
+    df = df.groupby("hour").mean()
+    return df / df.max()
+
+
+def compute_clusters(df, n_clusters=4):
+    """Compute station clusters based on bike availability time series
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input data, *i.e.* city-related timeseries, supposed to have
+    `station_id`, `ts` and `nb_bikes` columns
+    n_clusters : int
+        Number of desired clusters
+
+    Returns
+    -------
+    dict
+        Two pandas.DataFrame, the former for station clusters and the latter
+    for cluster centroids
+
+    """
+    df_norm = preprocess_data_for_clustering(df)
+    model = KMeans(n_clusters=n_clusters, random_state=0)
+    kmeans = model.fit(df_norm.T)
+    df_labels = pd.DataFrame(
+        {"id_station": df_norm.columns, "labels": kmeans.labels_}
+    )
+    df_centroids = pd.DataFrame(kmeans.cluster_centers_).reset_index()
+    return {"labels": df_labels, "centroids": df_centroids}
 
 
 class ComputeClusters(luigi.Task):
@@ -50,7 +114,7 @@ class ComputeClusters(luigi.Task):
             query, eng, params={"start": self.start, "stop": self.stop}
         )
         df.columns = ["station_id", "ts", "nb_bikes"]
-        clusters_ = clusters.compute_clusters(df, self.n_clusters)
+        clusters_ = compute_clusters(df, self.n_clusters)
         self.output().makedirs()
         path = self.output().path
         clusters_['labels'].to_hdf(path, '/clusters')
